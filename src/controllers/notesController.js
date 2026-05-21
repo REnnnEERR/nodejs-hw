1d8b4a2eede661a9636b6a2ad49fa361dfafc0c6
Ivan BOLTENKOV <ivanwrest@gmail.com>
2026-05-21 03:04:19 +0300

    done HW

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,6 +1,50 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
+
+export const getNotes = async (req, res) => {
+  const {
+    page = 1,
+    perPage = 10,
+    tag,
+    search,
+  } = req.query;
+
+  const skip = (page - 1) * perPage;
+  const limit = perPage;
+
+  const notesQuery = Note.find();
+
+  if (tag) {
+    notesQuery.where("tag").equals(tag);
+  }
+
+  if (search) {
+    notesQuery.where({
+      $text: {
+        $search: search,
+      },
+    });
+  }
+
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(limit),
+  ]);
+
+  const totalPages = Math.ceil(
+    totalNotes / limit
+  );
+
+  res.status(200).json({
+    page: Number(page),
+    perPage: Number(perPage),
+    totalNotes,
+    totalPages,
+    notes,
+  });
+};
+
 export const getAllNotes = async (req, res) => {
   // res.status(200).json({ message: "Retrieved all notes" });
   const notes = await Note.find();
